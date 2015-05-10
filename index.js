147104fe98d12a43e13ac0f1c4532b83d27ad496
Arthur Colle <hello@arthurcolle.com>
2015-05-10 00:38:31 -0400

    Added chat functionality

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+	socket.on('chat message', function(msg) {
+
+	});
 });
 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+   	io.emit('chat message', msg);
+  });
+});
 
 http.listen(3000, function() {
     console.log('listening on 3000');
